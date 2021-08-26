3eb6a07634f1ab3149f0af47956ab2e20730ded4
usna.hakimi95@gmail.com <usnahakimi@MA374.local>
2021-08-26 12:24:24 +0100

    cd changes 10

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
     apps: [
       {
-        name: 'usna-acebook',
-        script: 'npx',
-        args: 'serve -s build -l 3000 -n',
+        name: "usna-acebook",
+        script: "npm",
+        args: 'start',
         interpreter: 'none',
         env: {
-          NODE_ENV: 'development',
+          NODE_ENV: "production",
+          HOST: '0.0.0.0',
+          PORT: '3000',
         },
       },
     ],
